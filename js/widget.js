71942fab5806ee672d454b29ba6a3a86c3d6c7b7
felipe <felipemogollonr@gmail.com>
2016-03-21 23:19:16 +0100

    Hello world

---
+++ js/widget.js
@@ -0,0 +1,251 @@
+var MediaList = [ 
+	{
+		"id": "1194994855551224492_1121645817" , 
+		"created_time": "2016-02-28 18:20:52" ,
+		"media_text": "King lion \nPhoto by ©Fotografie-Egmond\n#wildlifeonearth" ,
+		"media_link": "https://www.instagram.com/p/BCVeX1hjMqs/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/e35/12716502_1696433940615289_1747830894_n.jpg",
+		"media_local_path": "img/12716502_1696433940615289_1747830894_n.jpg" ,
+		"username": "wildlifeonearth" ,
+		"full_name": "WILDLIFE|NATURE|CUTE|ANİMALS" ,
+		"tags":"lion"
+	},
+	{
+		"id": "1195164868090316846_1121645817" , 
+		"created_time": "2016-02-28 14:07:40" ,
+		"media_text": "Brown bear charging salmon in a remote Alaskan creek. | Photography by © Charles Glatzer\n\nTag #wildlifeonearth and follow us to be featured!" ,
+		"media_link": "https://instagram.com/p/7nSFQuP5u2/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/e35/12798017_839059569553536_107995133_n.jpg",
+		"media_local_path": "img/12798017_839059569553536_107995133_n.jpg" ,
+		"username": "wildlifeonearth" ,
+		"full_name": "WILDLIFE|NATURE|CUTE|ANİMALS" ,
+		"tags": "bear,river"
+	},
+	{
+		"id": "1192868096856869013_1121645817" , 
+		"created_time": "2016-02-25 17:03:03" ,
+		"media_text": "Adorable kitty & deer | Photography By ©chelseyslade" ,
+		"media_link": "https://www.instagram.com/p/BCN6zbJDMiV/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/e35/12751240_1076736925712637_957670009_n.jpg" ,
+		"media_local_path": "img/12751240_1076736925712637_957670009_n.jpg" ,
+		"username": "wildlifeonearth" ,
+		"full_name": "WILDLIFE|NATURE|CUTE|ANİMALS" ,
+		"tags":"cat,deer"
+	},
+	{
+		"id": "1192276663888300678_1121645817" , 
+		"created_time": "2016-02-24 14:25:23" ,
+		"media_text": "Haha 😂 great shot 👌 /Photography by © Christiane Slawik \n#wildlifeonearth" ,
+		"media_link": "https://www.instagram.com/p/BCL0U8RDMqG/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/e35/12749876_1030529670324359_47797938_n.jpg" ,
+		"media_local_path": "img/12749876_1030529670324359_47797938_n.jpg" ,
+		"username": "wildlifeonearth" ,
+		"full_name": "WILDLIFE|NATURE|CUTE|ANİMALS" ,
+		"tags":"dog, horse"
+	},
+	{
+		"id": "1192030263275473872_1121645817" , 
+		"created_time": "2016-02-23 20:18:12" ,
+		"media_text": "oohhhh, so nice!" ,
+		"media_link": "https://www.instagram.com/p/BCK8TV0DMvQ/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/e35/1170509_882580388526504_341127540_n.jpg" ,
+		"media_local_path": "img/1170509_882580388526504_341127540_n.jpg" ,
+		"username": "wildlifeonearth" ,
+		"full_name": "WILDLIFE|NATURE|CUTE|ANİMALS" ,
+		"tags": "lion, deer"
+	},
+	{
+		"id": "1194233531562948274_1295347780" , 
+		"created_time": "2016-02-28 21:40:36" ,
+		"media_text": "Polar bear with Cub. Mother love | Photograph by © Anton Belovodchenko @belovodchenko_anton\n\nTag #WildlifePlanet and follow us to be featured!" ,
+		"media_link": "https://www.instagram.com/p/BCSxRHTq3ay/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12783914_768306963302555_880929130_n.jpg" ,
+		"media_local_path": "img/12783914_768306963302555_880929130_n.jpg" ,
+		"username": "wildlifeplanet" ,
+		"full_name": "Animals - Wildlife" ,
+		"tags" : "bear"
+	},
+	{
+		"id": "1195049202324567223_1295347780" , 
+		"created_time": "2016-02-28 17:44:20" ,
+		"media_text": "Cubs | Photography by @ Chris Balcombe\n\nTag #WildlifePlanet and follow us to be featured!" ,
+		"media_link": "https://www.instagram.com/p/BCVqur5q3S3/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12728508_229847494027009_547216293_n.jpg" ,
+		"media_local_path": "img/12728508_229847494027009_547216293_n.jpg" ,
+		"username": "wildlifeplanet" ,
+		"full_name": "Animals - Wildlife" ,
+		"tags" : "tigers"
+	},	
+	{
+		"id": "1192771031294244836_1295347780" , 
+		"created_time": "2016-02-25 16:45:22" ,
+		"media_text": "Follow @nature.geography for the most breathtaking travel & nature photos! @nature.geography The life outdoors with @goldilocksandthewolf" ,
+		"media_link": "https://www.instagram.com/p/BCNku7yq3fk/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12750220_226730501007114_381364815_n.jpg" ,
+		"media_local_path": "img/12750220_226730501007114_381364815_n.jpg" ,
+		"username": "wildlifeplanet" ,
+		"full_name": "Animals - Wildlife" ,
+		"tags" : "wolf"
+	},
+	{
+		"id": "1191633264954799186_1295347780" , 
+		"created_time": "2016-02-23 17:23:45" ,
+		"media_text": "Follow @nature.geography for the most breathtaking travel & nature photos! @nature.geography The life outdoors with @goldilocksandthewolf" ,
+		"media_link": "https://www.instagram.com/p/BCJiCQUK3RS/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12748380_243261382674511_1323638054_n.jpg" ,
+		"media_local_path": "img/12748380_243261382674511_1323638054_n.jpg" ,
+		"username": "wildlifeplanet" ,
+		"full_name": "Animals - Wildlife" ,
+		"tags" : "lion, human"
+	},
+	{
+		"id": "1073976266169255864_268193584" , 
+		"created_time": "2015-08-14 16:26:09" ,
+		"media_text": "Nice dog kissing a fish" ,
+		"media_link": "https://www.instagram.com/p/8q9G5YlNVy/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/12142586_1668896966687635_239226796_n.jpg" ,
+		"media_local_path": "img/12142586_1668896966687635_239226796_n.jpg" ,
+		"username": "animaladicts" ,
+		"full_name": "Animaladicts",
+		"tags" : "dog, fish"
+	},
+	{
+		"id": "1092954877804729726_2201431332" , 
+		"created_time": "2015-07-26 17:12:12" ,
+		"media_text": "Polar bears swimming" ,
+		"media_link": "https://www.instagram.com/p/8q9KuvFNV-/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s480x480/e35/12142185_1625307984389098_706123523_n.jpg" ,
+		"media_local_path": "img/12142185_1625307984389098_706123523_n.jpg" ,
+		"username": "animaladicts" ,
+		"full_name": "Animaladicts",
+		"tags" : "bears"
+	},
+	{
+		"id": "1193887911545018172_194649484" , 
+		"created_time": "2016-02-27 17:11:51" ,
+		"media_text": "Dolphins, awesome!" ,
+		"media_link": "https://www.instagram.com/p/BCRirrlRhc8/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12797598_1705532343048073_475838813_n.jpg" ,
+		"media_local_path": "img/12797598_1705532343048073_475838813_n.jpg" ,
+		"username": "instaanimal" ,
+		"full_name": "Animals" ,
+		"tags" : "dolphins"
+	},
+	{
+		"id": "1193430900810323792_194649484" , 
+		"created_time": "2016-02-26 17:10:21" ,
+		"media_text": "Incredible Tiger!" ,
+		"media_link": "https://www.instagram.com/p/BCP6xTKxhdQ/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/1390255_962445990458127_1862282099_n.jpg" ,
+		"media_local_path": "img/1390255_962445990458127_1862282099_n.jpg" ,
+		"username": "instaanimal" ,
+		"full_name": "Animals" ,
+		"tags" : "tiger,jungle"
+	},
+	{
+		"id": "1191614261219431597_194649484" , 
+		"created_time": "2016-02-23 17:00:06" ,
+		"media_text": "Wow! Animals Love!" ,
+		"media_link": "https://www.instagram.com/p/BCJdtttRhSt/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12728664_1069143976441638_1568988439_n.jpg" ,
+		"media_local_path": "img/12728664_1069143976441638_1568988439_n.jpg" ,
+		"username": "instaanimal" ,
+		"full_name": "Animals" ,
+		"tags" : "horse, cat "
+	},
+	{
+		"id": "1192448236292805638_194649484" , 
+		"created_time": "2016-02-21 16:42:51" ,
+		"media_text": "Cool, bear swimming!" ,
+		"media_link": "https://www.instagram.com/p/BCMbVphRhQG/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/11355999_956713737739169_275804961_n.jpg" ,
+		"media_local_path": "img/11355999_956713737739169_275804961_n.jpg" ,
+		"username": "instaanimal" ,
+		"full_name": "Animals" ,
+		"tags" : "bear"
+	},
+	{
+		"id": "1186807640439703071_2103172096" , 
+		"created_time": "2016-02-19 16:36:24" ,
+		"media_text": "Brothers" ,
+		"media_link": "https://www.instagram.com/p/BB4Y0LRIMYf/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12627877_430395187150638_1660498281_n.jpg" ,
+		"media_local_path": "img/12627877_430395187150638_1660498281_n.jpg" ,
+		"username": "animaldisplay" ,
+		"full_name": "Animals" ,
+		"tags" : "dogs"
+	},
+	{  
+		"id": "1186363941008229921_2103172096" , 
+		"created_time": "2016-02-16 16:03:39" ,
+		"media_text": "So majestic!" ,
+		"media_link": "https://www.instagram.com/p/BB2z7f-IMYh/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12717116_1246153612080833_988550396_n.jpg" ,
+		"media_local_path": "img/12717116_1246153612080833_988550396_n.jpg" ,
+		"username": "animaldisplay" ,
+		"full_name": "Animals" ,
+		"tags":"lions"
+	},
+	{
+		"id": "1183989649230578720_2103172096" , 
+		"created_time": "2016-02-14 15:28:19" ,
+		"media_text": "Adorable Dalmatian puppy" ,
+		"media_link": "https://www.instagram.com/p/BBuYFAcIMQg/" ,
+		"media_image_link": "https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/12479569_160603460987212_906351994_n.jpg" ,
+		"media_local_path": "img/12479569_160603460987212_906351994_n.jpg" ,
+		"username": "animaldisplay" ,
+		"full_name": "Animals" ,
+		"tags":"dogs"
+	}];
+	function sayName(){alert("hola")};
+	function searchUser()
+	{
+		var userTextField=document.getElementById("usuari");
+		var user=userTextField.value;
+
+
+		var userInfo=[];
+
+		for (var i=0; i<MediaList.length; i++) {
+    		if ( MediaList[i].username == user ) {
+        		userInfo.push(MediaList[i]);
+        		//break;
+    		}
+		}
+
+		console.log("There are "+userInfo.length+" entries with name "+user);
+		printElements(userInfo);
+
+	};
+	function printElements(object)
+	{
+		var i;
+
+		var widget=document.getElementById("widget");
+
+//TODO comprobar si tienen que ser repetidos o no
+//TODO cortar la fecha para que no aparezca la hora
+		for(i=0;i<object.length;i++)
+		{
+			var li = document.createElement("li");
+			var src = document.createElement("img");
+			src.setAttribute("src",object[i].media_local_path);
+			var post_date = document.createElement("div");
+			post_date.setAttribute("class","post_date");
+			var date=object[i].created_time.split(" ");
+			post_date.innerHTML=date[0];
+			var author = document.createElement("div");
+			author.setAttribute("class","author");
+			author.innerHTML=object[i].username;
+			var descr = document.createElement("div");
+			descr.setAttribute("class","descr");
+			descr.innerHTML=object[i].media_text;
+			li.appendChild(src);
+			li.appendChild(post_date);
+			li.appendChild(author);
+			li.appendChild(descr);
+
+			widget.appendChild(li);
+		}
+	};
+
